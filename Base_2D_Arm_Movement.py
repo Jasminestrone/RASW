052b22e844fbd7e882c83ce291e21bd9a9f554d6
Un3l3phant <prt271828@gmail.com>
2025-04-07 14:27:39 -0400

    Fixed and tested math
    It works!!!

--- Base_2D_Arm_Movement.py
+++ Base_2D_Arm_Movement.py
@@ -1,8 +1,8 @@
 import math
 
 # Arm lengths
-L1 = 10
-L2 = 10
+L1 = 160
+L2 = 160
 
 
 def arm_math(x, y, L1, L2):
@@ -51,17 +51,21 @@ print(f"Elbow Angle: {elbow_angle:.2f} degrees")
 # How the math works
 
 # The elbow angle is found using the law of cosines where we do
-# cos(theta2) = (D^2 - L1^2 - L2^2) / 2L1L2
-# Then we can find the elbow angle usign the equation
+# cos(theta2) = (L1^2 + L2^2 - D^2) / (2*L1*L2)
+# Then we can find the elbow angle using the equation
 # theta2 = cos^-1(cos(theta2))
-# This outputs the angle that the eblow arm needs to be at
+# This outputs the angle that the elbow arm needs to be at (where 0 degrees is fully extended
+# and 180 degrees is fully folded back)
 
-# Next we can compute the shoudler angle
-# First we can break the problem into two different angles
-# a = tan^-1(y/x) Which is the angle fro mthe origin to the target point
-# b = tan^-1((L2 sin(theta2)) / (L1 + L2 cos(theta2))) Which is the internal angle being caused by the forearm
-# Finnaly if we can get theta1 by doing
-# theta1 = a - b
+# Next we compute the shoulder angle in two steps:
+# 1. First we find the angle from the origin to the target point:
+#    a = tan^-1(y/x)
+# 2. Then we find the angle between the first link and the line to the target:
+#    cos(alpha) = (L1^2 + D^2 - L2^2) / (2*L1*D)
+#    alpha = cos^-1(cos(alpha))
 
-# Then simply we can convet these angles from radians to degrees by doing
+# Finally we get theta1 by subtracting alpha from the target angle:
+# theta1 = a - alpha
+
+# We convert these angles from radians to degrees by doing:
 # theta1_degs = theta1 * 180/pi and theta2_degs = theta2 * 180/pi
